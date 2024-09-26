ef849b5b73ef5b8773c9abe32d368fb8bd00468b
Joe Politz <joe.politz@gmail.com>
2024-09-26 22:28:38 +0000

    hellobin

---
+++ hellobin.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char hello[] = "Hello!";
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello3[] = { 0b1001000, 0b1100101, 0b1101100, 0b1101100, 0b1101111, 0b100001, 0b0 };
+
+  puts(hello);
+  puts(hello2);
+  puts(hello3);
+}
